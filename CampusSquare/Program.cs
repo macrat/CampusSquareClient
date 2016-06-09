93fe07cad88de19af37a5c13e8c9714541ce8f10
5st7 <5st7@not404friends@gmail.com>
2016-06-09 12:25:54 +0900

    test

---
+++ CampusSquare/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CampusSquare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ICampusSquareClient cam = new CampusSuquareClient("c01144495d", "6qa4Jg%Kq");
+            Console.WriteLine(cam.GetGradePage());
+            Console.ReadLine();
+        }
+    }
+}
