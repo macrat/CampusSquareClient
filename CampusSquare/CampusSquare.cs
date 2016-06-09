b32300b9bdfb27afcbec0cd8fcc01530c47b2683
MacRat <m@crat.jp>
2016-06-09 18:27:02 +0900

    Added exceptions

--- CampusSquare/CampusSquare.cs
+++ CampusSquare/CampusSquare.cs
@@ -18,7 +18,6 @@ namespace CampusSquare {
 public class CampusSquare : AbcCampusSquare {
 	/// <param name="userName">CampusSquareのユーザー名</param>
 	/// <param name="password">CampusSquareにログインする時に使用するパスワード</param>
-	/// <exception cref="System.Security.Authentication.AuthenticationException">認証に失敗した場合に送出されます。</exception>
 	public CampusSquare(string userName, string password) {
 		client = new CampusSquareClient(userName, password);
 		parser = new GradeParser();
