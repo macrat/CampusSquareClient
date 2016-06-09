6b8bb699d8a5398189386eed101f8b0cf4ef7c3b
MacRat <m@crat.jp>
2016-06-09 11:54:29 +0900

    Re-implemented CampusSquare and Added DummyCampusSquare

---
+++ CampusSquare/AbcCampusSquare.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+
+
+namespace CampusSquare {
+
+/// <summary>
+/// ICampusSquareを実装するための共通っぽい仕様をまとめたもの。
+/// </summary>
+abstract class AbcCampusSquare : ICampusSquare {
+	protected ICampusSquareClient client;
+	protected IGradeParser parser;
+
+
+	/// <summary>
+	/// 今までの全ての成績を取得する。
+	/// </summary>
+	public IEnumerable<IGrade> GetGrades() {
+		return parser.Parse(client.GetGradePage());
+	}
+
+	/// <summary>
+	/// ある年度の成績だけを取得する。
+	/// </summary>
+	/// <param name="year">取得する年度</param>
+	public IEnumerable<IGrade> GetGrades(int year) {
+		return parser.Parse(client.GetGradePage(year));
+	}
+
+	/// <summary>
+	/// ある年度の前期だけもしくは後期だけの成績を取得する。
+	/// </summary>
+	/// <param name="year">取得する年度</param>
+	/// <param name="firstHalf">trueなら前期、falseなら後期</param>
+	public IEnumerable<IGrade> GetGrades(int year, bool firstHalf) {
+		return parser.Parse(client.GetGradePage(year, firstHalf));
+	}
+}
+
+}
--- CampusSquare/CampusSquare.cs
+++ CampusSquare/CampusSquare.cs
@@ -15,40 +15,12 @@ namespace CampusSquare {
 /// }
 /// </code>
 /// </example>
-class CampusSquare {
-	private CampusSquareClient client;
-	private GradeParser parser;
-
-
+class CampusSquare : AbcCampusSquare {
 	/// <param name="userName">CampusSquareのユーザー名</param>
 	/// <param name="password">CampusSquareにログインする時に使用するパスワード</param>
 	public CampusSquare(string userName, string password) {
 		client = new CampusSquareClient(userName, password);
 		parser = new GradeParser();
-	}
-
-	/// <summary>
-	/// 今までの全ての成績を取得する。
-	/// </summary>
-	IEnumerable<IGrade> GetGrades() {
-		return parser.Parse(client.GetGradePage());
-	}
-
-	/// <summary>
-	/// ある年度の成績だけを取得する。
-	/// </summary>
-	/// <param name="year">取得する年度</param>
-	IEnumerable<IGrade> GetGrades(int year) {
-		return parser.Parse(client.GetGradePage(year));
-	}
-
-	/// <summary>
-	/// ある年度の前期だけもしくは後期だけの成績を取得する。
-	/// </summary>
-	/// <param name="year">取得する年度</param>
-	/// <param name="firstHalf">trueなら前期、falseなら後期</param>
-	IEnumerable<IGrade> GetGrades(int year, bool firstHalf) {
-		return parser.Parse(client.GetGradePage(year, firstHalf));
 	}
 }
 
