6ece538575f584036b837335cd749bf5852c40d4
MacRat <m@crat.jp>
2016-06-09 18:16:20 +0900

    Refactored @5st7's great class

--- CampusSquare/CampusSquareClient.cs
+++ CampusSquare/CampusSquareClient.cs
@@ -11,88 +11,80 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
+
 namespace CampusSquare
 {
     class CampusSquareClient : ICampusSquareClient
     {
-        private const string GET_GRADES_URL = "https://kyo-web.teu.ac.jp/campusweb/campussquare.do?_flowId=SIW0001300-flow";
-        private const string LOGIN_URL = "https://kyo-web.teu.ac.jp/campusweb/campussquare.do";
-        private string flowExecutionKey;
-        CustomWebClient client ;
+        private const string URL = "https://kyo-web.teu.ac.jp/campusweb/campussquare.do";
+
+        private CustomWebClient client;
 
 
-        public CampusSquareClient(string userName,string password)
+        public CampusSquareClient(string userName, string password)
         {
             client = new CustomWebClient();
-            client = getCookkie(userName,password);
 
+            Login(userName, password);
         }
 
-        public string GetGradePage()
+        private void Login(string userName, string password)
         {
-            client.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.116 Safari/537.36");
-            client.Encoding = Encoding.UTF8;
-
-            var data = client.UploadValues(LOGIN_URL, new NameValueCollection {
-                { "status", "1" },
-                { "_flowExecutionKey", flowExecutionKey},
-                { "_eventId", "display"},
-                { "spanType", "0"},
-                { "nendo", "2016"},
-                { "gakkiKbnCd","1"}
-            });
-
-            var res = System.Text.Encoding.UTF8.GetString(data);
-            return res;
-        }
-
-
-        public string GetGradePage(int year, bool firstHalf)
-        {
-            client.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.116 Safari/537.36");
-            client.Encoding = Encoding.UTF8;
-
-            var data = client.UploadValues(LOGIN_URL, new NameValueCollection {
-                { "status", "1" },
-                { "_flowExecutionKey", flowExecutionKey},
-                { "_eventId", "display"},
-                { "spanType", "0"},
-                { "nendo", year.ToString()},
-                { "gakkiKbnCd", firstHalf ? "1" : "0"}
-            });
-
-            var res = System.Text.Encoding.UTF8.GetString(data);
-            return res;
-        }
-
-        private CustomWebClient getCookkie(string userName, string password)
-        {
-            client.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.116 Safari/537.36");
-            client.Encoding = Encoding.UTF8;
-
-            client.UploadValues(LOGIN_URL, new NameValueCollection {
+            client.Post(URL, new NameValueCollection {
                 { "locale", "ja_JP" },
                 { "_flowId", "USW0009000-flow"},
                 { "requestparames", ""},
                 { "userName", userName},
                 { "password", password},
             });
+        }
 
-            var result = client.DownloadString(GET_GRADES_URL);
+        private string GetFlowExecutionKey()
+        {
+            var result = client.Get(URL + "?_flowId=SIW0001300-flow");
 
-			if(result.Contains("認証エラー")){
-				throw new AuthenticationException();
-			}
+            if(result.Contains("認証エラー"))
+            {
+                throw new AuthenticationException();
+            }
 
-            Match match = Regex.Match(result, "<input type=\"hidden\" name=\"_flowExecutionKey\" value=\"(.+?)\">");
-            match = Regex.Match(match.Value, "(?<=value=\").+?(?=\")");
-            flowExecutionKey = match.Value;
-            return client;
+            return Regex.Match(
+                result,
+                "<input type=\"hidden\" name=\"_flowExecutionKey\" value=\"(.+?)\">"
+            ).Groups[1].Value;
+        }
+
+        public string GetGradePage()
+        {
+            return GetGradePage(new NameValueCollection {
+                {"spanType", "0"}
+            });
+        }
+
+        public string GetGradePage(int year, bool firstHalf)
+        {
+            return GetGradePage(new NameValueCollection {
+                {"spanType", "1"},
+                {"nendo", year.ToString()},
+                {"gakkiKbnCd", firstHalf ? "1" : "2"}
+            });
+        }
+
+        private string GetGradePage(NameValueCollection argument)
+        {
+            argument.Add("status", "1");
+            argument.Add("_flowExecutionKey", GetFlowExecutionKey());
+            argument.Add("_eventId", "display");
+
+            return client.Post(URL, argument);
         }
     }
+
+
     class CustomWebClient : WebClient
     {
         private CookieContainer cookieContainer = new CookieContainer();
+
         protected override WebRequest GetWebRequest(Uri address)
         {
             var request = base.GetWebRequest(address);
@@ -101,6 +93,26 @@ namespace CampusSquare
                 (request as HttpWebRequest).CookieContainer = cookieContainer;
             }
             return request;
+        }
+
+        public void InitializeHeader()
+        {
+            Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.116 Safari/537.36");
+            Encoding = Encoding.UTF8;
+        }
+
+        public string Post(string address, NameValueCollection data)
+        {
+            InitializeHeader();
+            return System.Text.Encoding.UTF8.GetString(
+                UploadValues(address, data)
+            );
+        }
+
+        public string Get(string address)
+        {
+            InitializeHeader();
+            return DownloadString(address);
         }
     }
 }
